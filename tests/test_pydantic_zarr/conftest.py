e46db6accdace39f1208a470ea9bade085325e4e
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-07-28 13:45:08 +0200

    add fill values to dtype examples test fixtures

--- tests/test_pydantic_zarr/conftest.py
+++ tests/test_pydantic_zarr/conftest.py
@@ -7,49 +7,50 @@ from typing import Any
 from packaging.version import Version
 
 ZARR_PYTHON_VERSION = Version(version("zarr"))
+DTYPE_EXAMPLES_V2: tuple[tuple[Any, Any], ...]
+DTYPE_EXAMPLES_V3: tuple[tuple[Any, Any], ...]
 
 if ZARR_PYTHON_VERSION < Version("3.1.0"):
-    DTYPE_EXAMPLES = ()
-    DTYPE_EXAMPLES_V2 = [
-        "|b1",
-        "|i1",
-        "<i2",
-        "<i4",
-        "<i8",
-        "|u1",
-        "<u2",
-        "<u4",
-        "<u8",
-        "<f2",
-        "<f4",
-        "<f8",
-        "<c8",
-        "<c16",
-        "<U10",
-        "|O",
-        "|V10",
-        "|S10",
-        [("a", "<i4"), ("b", "<f2")],
-        "<M8[10s]",
-        "<m8[10s]",
-    ]
-    DTYPE_EXAMPLES_V3 = [
-        "bool",
-        "int8",
-        "int16",
-        "int32",
-        "int64",
-        "uint8",
-        "uint16",
-        "uint32",
-        "uint64",
-        "float16",
-        "float32",
-        "float64",
-        "complex64",
-        "complex128",
-        "str",
-    ]
+    DTYPE_EXAMPLES_V2 = (
+        ("|b1", True),
+        ("|i1", -1),
+        ("|i2", -1),
+        ("|i4", -1),
+        ("|i8", -1),
+        ("|u1", 1),
+        ("<u2", 1),
+        ("<u4", 1),
+        ("<u8", 1),
+        ("<f2", 1.0),
+        ("<f4", 1.0),
+        ("<f8", 1.0),
+        ("<c8", [1.0, 1.0]),
+        ("<c16", [1.0, 10]),
+        ("<U10", "abcdefghij"),
+        ("|O", "hi"),
+        ("|V10", "AAAAAAAAAAAAAA=="),
+        ("|S10", "AAAAAAAAAAAAAA=="),
+        ([("a", "<i4"), ("b", "<f2")], "AAAAAAAA"),
+        ("<M8[10s]", "NaT"),
+        ("<m8[10s]", "NaT"),
+    )
+    DTYPE_EXAMPLES_V3 = (
+        ("bool", True),
+        ("int8", -1),
+        ("int16", -1),
+        ("int32", -1),
+        ("int64", -1),
+        ("uint8", 1),
+        ("uint16", 1),
+        ("uint32", 1),
+        ("uint64", 1),
+        ("float16", 1.0),
+        ("float32", 1.0),
+        ("float64", 1.0),
+        ("complex64", [1, 1]),
+        ("complex128", [1, 1]),
+        ("str", "hi"),
+    )
 else:
     from zarr.core.dtype import (
         DateTime64,
@@ -60,23 +61,33 @@ else:
         RawBytes,
         Structured,
         TimeDelta64,
-        ZDType,
         data_type_registry,
     )
 
-    DTYPE_EXAMPLES: tuple[ZDType[Any, Any], ...] = ()
-
+    v2_examples = []
+    v3_examples = []
     for dtype_cls in data_type_registry.contents.values():
         if dtype_cls in (DateTime64, TimeDelta64):
-            DTYPE_EXAMPLES += (dtype_cls(unit="s", scale_factor=10),)
+            dt = dtype_cls(unit="s", scale_factor=10)
         elif dtype_cls in (FixedLengthUTF32, RawBytes, NullTerminatedBytes):
-            DTYPE_EXAMPLES += (dtype_cls(length=10),)
+            dt = dtype_cls(length=10)
         elif dtype_cls == Structured:
-            DTYPE_EXAMPLES += (dtype_cls(fields=[("a", Int32()), ("b", Float16())]),)
+            dt = dtype_cls(fields=[("a", Int32()), ("b", Float16())])
         else:
-            DTYPE_EXAMPLES += (dtype_cls(),)
-    DTYPE_EXAMPLES_V2 = [d.to_json(zarr_format=2)["name"] for d in DTYPE_EXAMPLES]
-    # Suppress the userwarning emitted when creating off-spec dtypes
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", FutureWarning)
-        DTYPE_EXAMPLES_V3 = [d.to_json(zarr_format=3) for d in DTYPE_EXAMPLES]
+            dt = dtype_cls()
+
+        v2_examples.append(
+            (
+                dt.to_json(zarr_format=2)["name"],
+                dt.to_json_scalar(dt.default_scalar(), zarr_format=2),
+            )
+        )
+        # Suppress the userwarning emitted when creating off-spec dtypes
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", FutureWarning)
+            v3_examples.append(
+                (dt.to_json(zarr_format=3), dt.to_json_scalar(dt.default_scalar(), zarr_format=3))
+            )
+
+    DTYPE_EXAMPLES_V2 = tuple(v2_examples)
+    DTYPE_EXAMPLES_V3 = tuple(v3_examples)
--- tests/test_pydantic_zarr/test_v3.py
+++ tests/test_pydantic_zarr/test_v3.py
@@ -67,8 +67,8 @@ def test_from_array() -> None:
 @pytest.mark.filterwarnings("ignore:The dtype:UserWarning")
 @pytest.mark.filterwarnings("ignore:The data type:FutureWarning")
 @pytest.mark.filterwarnings("ignore:The codec:UserWarning")
-@pytest.mark.parametrize("data_type", DTYPE_EXAMPLES_V3, ids=str)
-def test_arrayspec_from_zarr(data_type: object) -> None:
+@pytest.mark.parametrize(("data_type", "fill_value"), DTYPE_EXAMPLES_V3, ids=str)
+def test_arrayspec_from_zarr(data_type: object, fill_value: object) -> None:
     """
     Test that deserializing an ArraySpec from a zarr python store works as expected.
     """
@@ -77,6 +77,7 @@ def test_arrayspec_from_zarr(data_type: object) -> None:
         pytest.skip(
             reason="Bug in zarr python: see https://github.com/zarr-developers/zarr-python/issues/3263"
         )
+
     arr = zarr.create_array(store=store, shape=(10,), dtype=data_type, zarr_format=3)
 
     arr_spec = ArraySpec.from_zarr(arr)
@@ -87,19 +88,33 @@ def test_arrayspec_from_zarr(data_type: object) -> None:
 
 @pytest.mark.parametrize("path", ["", "foo"])
 @pytest.mark.parametrize("overwrite", [True, False])
+@pytest.mark.parametrize(("data_type", "fill_value"), DTYPE_EXAMPLES_V3, ids=str)
 @pytest.mark.parametrize("config", [{}, {"write_empty_chunks": True, "order": "F"}])
-def test_arrayspec_to_zarr(path: str, overwrite: bool, config: dict[str, object]) -> None:
+@pytest.mark.filterwarnings("ignore:The codec `vlen-utf8`:UserWarning")
+@pytest.mark.filterwarnings("ignore:The codec `vlen-bytes`:UserWarning")
+@pytest.mark.filterwarnings("ignore:The data type :FutureWarning")
+def test_arrayspec_to_zarr(
+    path: str, overwrite: bool, config: dict[str, object], data_type: object, fill_value: object
+) -> None:
     """
     Test that serializing an ArraySpec to a zarr python store works as expected.
     """
+    codecs = ({"name": "bytes", "configuration": {}},)
+    if data_type == "variable_length_bytes":
+        codecs = ({"name": "vlen-bytes"},)
+
+    elif data_type in ("str", "string"):
+        codecs = ({"name": "vlen-utf8"},)
+
     store = {}
+
     arr_spec = ArraySpec(
         shape=(10,),
-        data_type="uint8",
+        data_type=data_type,
         chunk_grid={"name": "regular", "configuration": {"chunk_shape": (10,)}},
         chunk_key_encoding={"name": "default", "configuration": {"separator": "/"}},
-        codecs=({"name": "bytes", "configuration": {}},),
-        fill_value=0,
+        codecs=codecs,
+        fill_value=fill_value,
         dimension_names=("x",),
     )
     arr = arr_spec.to_zarr(store=store, path=path, overwrite=overwrite, config=config)
